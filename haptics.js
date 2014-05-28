3b6ae11df2105984b09b05e9aeea8cdaa54755f1
Shantanu Bala <shantanubala@gmail.com>
2014-05-28 13:54:01 -0700

    added comments, fixed code

--- haptics.js
+++ haptics.js
@@ -12,37 +12,44 @@
 (function () {
     var Haptics = {},
         enabled,
+        vibrate,
         currentRecording,
         log,
         durations;
 
+    // string-integer mapping of durations for more readable code
     durations = {
         'slow': 250,
         'medium': 500,
         'fast': 750
     };
 
+    // simple mapper function for getting string durations
     function getDuration (d) {
         return durations[d] || d;
     }
 
+    // a console.log wrapper for debugging
     log = function(){
-        log.history = log.history || [];   // store logs to an array for reference
+        // store logs to an array for reference
+        log.history = log.history || [];
         log.history.push(arguments);
         if(this.console){
             console.log( Array.prototype.slice.call(arguments) );
         }
     };
 
+    // check for navigator variables from different vendors
     enabled = vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
 
+    // expose a wrapped copy of 'vibrate' function
     if (enabled) {
         Haptics.vibrate = function () {
             var params = [], i, len;
             for (i = 0, len = arguments.length; i < len; i += 1) {
                 params.push(getDuration(arguments[i]));
             }
-            navigator.vibrate.apply(this, params);
+            vibrate.apply(this, params);
             return true;
         };
     }
@@ -52,10 +59,12 @@
         };
     }
 
+    // handle click/touch event
     function onRecord(e) {
         e.preventDefault();
         currentRecording.push(new Date());
     }
+    // begin recording a sequence of taps/clicks
     function record() {
         currentRecording = [];
         window.addEventListener("touchstart". onRecord, false);
@@ -63,6 +72,7 @@
         window.addEventListener("mousedown", onRecord, false);
         window.addEventListener("mouseup". onRecord, false);
     }
+    // complete a recording of a sequence of taps/clicks
     function finish() {
         log(currentRecording);
         window.removeEventListener("touchstart". onRecord);
@@ -72,9 +82,15 @@
         return currentRecording;
     }
 
+    // expose local functions to global API
     Haptics.enabled = enabled;
     Haptics.record = record;
     Haptics.finish = finish;
     Haptics.durations = durations;
+
+    // set global object
     this.Haptics = Haptics;
+    if (window) {
+        window.Haptics = Haptics;
+    }
 })();
