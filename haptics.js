f210a21f929e30414bcca7f18d9b42fa75b36bf6
Shantanu Bala <shantanubala@gmail.com>
2014-05-30 19:01:22 -0700

    added fadeIn effect

--- haptics.js
+++ haptics.js
@@ -13,51 +13,36 @@
         enabled,
         vibrate,
         currentRecording,
-        log,
-        durations;
+        log;
 
-    // string-integer mapping of durations for more readable code
-    durations = {
-        'slow': 250,
-        'medium': 500,
-        'fast': 750
-    };
-
-    // simple mapper function for getting string durations
-    function getDuration (d) {
-        return durations[d] || d;
-    }
+    Haptics.resolution = 10; // lower is sharper
 
     // a console.log wrapper for debugging
     log = function(){
         // store logs to an array for reference
         log.history = log.history || [];
         log.history.push(arguments);
-        if(this.console){
-            console.log( Array.prototype.slice.call(arguments) );
+        if(global.console){
+            global.console.log( Array.prototype.slice.call(arguments) );
         }
     };
 
     // check for navigator variables from different vendors
     enabled = vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
 
+    function executeSequence(durations, currentFunc, nextFunc) {
+        nextFunc = nextFunc || currentFunc;
+        var d = durations.shift();
+        currentFunc(d);
+        return window.setTimeout(function () {
+            executeSequence(durations, nextFunc, currentFunc);
+        }, d);
+    }
+
     // expose a wrapped copy of 'vibrate' function
     if (enabled) {
         Haptics.vibrate = function (args) {
-            var params = [], i, len;
-            if (typeof args == "number") {
-                vibrate(args);
-                return true;
-            }
-
-            if (args.length) {
-                for (i = 0, len = args.length; i < len; i += 1) {
-                    params.push(getDuration(args[i]));
-                }
-                vibrate.apply(this, params);
-                return true;
-            }
-
+            vibrate(args);
             return false;
         };
     }
@@ -65,6 +50,11 @@
         Haptics.vibrate = function () {
             return false;
         };
+    }
+
+    // used for timeouts that 'accomplish nothing' in a pattern
+    function emptyFunc() {
+        // do nothing
     }
 
     // handle click/touch event
@@ -104,6 +94,29 @@
         }
 
         return vibrationPattern;
+    }
+
+    // EFFECTS: Fade In
+    function vibrateFadeIn(duration) {
+        var pulses = [Haptics.resolution],
+            len = duration / Haptics.resolution,
+            resolution = Haptics.resolution,
+            i;
+        for (i = 1; i <= len; i += 1) {
+            pulses.push(resolution);
+            pulses.push(i * resolution);
+        }
+        pulses.reverse();
+        vibrate(pulses);
+    }
+
+    function fadeIn(args) {
+        if (typeof args == "number") {
+            vibrateFadeIn(args);
+        }
+        else {
+            executeSequence(args, vibrateFadeIn, emptyFunc);
+        }
     }
 
     // expose local functions to global API
